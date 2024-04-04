89048b3dc9be625efe4ef673dea723eeaea38ab2
brooklinbecker <149913166+brooklinbecker@users.noreply.github.com>
2024-04-03 22:38:17 -0400

    adding content

---
+++ scripts/00-simulate_data.R
@@ -0,0 +1,19 @@
+#### Preamble ####
+# Purpose: Simulates... [...UPDATE THIS...]
+# Author: Brooklin Becker 
+# Date: 3 April 2024 [...UPDATE THIS...]
+# Contact: brooklin.becker@mail.utoronto.ca 
+# License: MIT
+# Pre-requisites: [...UPDATE THIS...]
+# Any other information needed? [...UPDATE THIS...]
+
+
+#### Workspace setup ####
+library(tidyverse)
+# [...UPDATE THIS...]
+
+#### Simulate data ####
+# [...ADD CODE HERE...]
+
+
+
---
+++ scripts/03-test_data.R
@@ -0,0 +1,15 @@
+#### Preamble ####
+# Purpose: Tests... [...UPDATE THIS...]
+# Author: Brooklin Becker 
+# Date: 3 April 2024 [...UPDATE THIS...]
+# Contact: brooklin.becker@mail.utoronto.ca 
+# License: MIT
+# Pre-requisites: [...UPDATE THIS...]
+# Any other information needed? [...UPDATE THIS...]
+
+
+#### Workspace setup ####
+library(tidyverse)
+# [...UPDATE THIS...]
+
+#### Test data ####
